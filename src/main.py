407e5e316f0e92068ec7cea6b883f3640a883a93
Cal Blanco <calblanco1125@gmail.com>
2024-11-13 10:56:38 -0800

    added in an example of going from NER baseline to query results

--- src/main.py
+++ src/main.py
@@ -3,5 +3,11 @@ from named_entity_recognition import ner_baseline
 from database import query_by_name, query_by_instance, query_by_id
 
 
-print(query_by_id(['Q49'], 'name, instance_of'))
+out = ner_baseline("fr", num_samples=10) #generate NER baseline results 
+print(out)
 
+ids = [] #Create a list of just the ids we should have recieved
+for item in out:
+    ids.extend(item[1])
+
+print(query_by_id(ids, 'wiki_id, name, instance_of, fr')) #print out query results
