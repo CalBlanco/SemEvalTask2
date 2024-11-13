330b72081e9f420d23232643b42d4fe3d19ba48e
Cal Blanco <wcblanco@ucsc.edu>
2024-11-13 10:50:09 -0800

    Added in a function to query by ids over the sql db

--- src/main.py
+++ src/main.py
@@ -1,14 +1,7 @@
 from data_util import retrieve_data
 from named_entity_recognition import ner_baseline
-from database import command, query, query_by_name, query_by_instance
+from database import query_by_name, query_by_instance, query_by_id
 
-# print(command('PRAGMA table_info(entity_translation)')) view columns of db
 
-# print(query('name, ar, fr', 'WHERE instance_of = "human" AND name LIKE "la%"')) returns the (name, arabic_translation, french_translation) for humans that have a name la* where * represents any amount of anycharacters 
+print(query_by_id(['Q49'], 'name, instance_of'))
 
-print('Query by instance\n')
-#query 5 humans from the table and get their ids, names, and spanish translations
-print(query_by_instance("human", "wiki_id, name, es", "LIMIT 5")) 
-print('\n\nQuery by Name')
-# query for names starting in La, getting their id, name, instance, german, spanish and japanese translation (only return 4)
-print(query_by_name("La%", 'wiki_id, name, instance_of, de, es, ja', 'LIMIT 4')) 
