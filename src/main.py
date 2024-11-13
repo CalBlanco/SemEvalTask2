1750fdf987ae567ea9b012deb9120b66f076910d
Cal Blanco <carl@Mac.attlocal.net>
2024-11-13 00:51:21 -0800

    moved comments aroud nbd

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from database import command, query, query_by_name, query_by_instance
 # print(query('name, ar, fr', 'WHERE instance_of = "human" AND name LIKE "la%"')) returns the (name, arabic_translation, french_translation) for humans that have a name la* where * represents any amount of anycharacters 
 
 print('Query by instance\n')
-print(query_by_instance("human", "wiki_id, name, es", "LIMIT 5")) #query 5 humans from the table and get their ids, names, and spanish translations
+#query 5 humans from the table and get their ids, names, and spanish translations
+print(query_by_instance("human", "wiki_id, name, es", "LIMIT 5")) 
 print('\n\nQuery by Name')
-print(query_by_name("La%", 'wiki_id, name, instance_of, de, es, ja', 'LIMIT 4')) # query for names starting in La, getting their id, name, instance, german, spanish and japanese translation (only return 4)
+# query for names starting in La, getting their id, name, instance, german, spanish and japanese translation (only return 4)
+print(query_by_name("La%", 'wiki_id, name, instance_of, de, es, ja', 'LIMIT 4')) 
