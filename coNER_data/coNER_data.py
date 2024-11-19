cf40a747916922fbb629e53ae66e433175f300b2
elmchelsey <chelseygelmore@gmail.com>
2024-11-19 09:04:11 -0800

    downloaded en_coNER data and wrote trainvaltest split to json files

--- coNER_data/coNER_data.py
+++ coNER_data/coNER_data.py
@@ -10,11 +10,12 @@ def save_to_json(data, file):
 
 train_data = [{"id": instance["id"], "sample_id": instance["sample_id"], "tokens": instance["tokens"], "ner_tags": instance["ner_tags"], "ner_tags_index": instance["ner_tags_index"]} for instance in en_dataset["train"]]
 validation_data = [{"id": instance["id"], "sample_id": instance["sample_id"], "tokens": instance["tokens"], "ner_tags": instance["ner_tags"], "ner_tags_index": instance["ner_tags_index"]} for instance in en_dataset["validation"]]
-test_data = [{"id": instance["id"], "sample_id": instance["sample_id"], "tokens": instance["tokens"], "ner_tags": instance["ner_tags"], "ner_tags_index": instance["ner_tags_index"]} for instance in en_dataset["test"]]
 
+test_data = [
+    {"id": instance["id"], "sample_id": instance["sample_id"], "tokens": instance["tokens"], "ner_tags": instance["ner_tags"], "ner_tags_index": instance["ner_tags_index"]}
+    for i, instance in enumerate(en_dataset["test"]) if i < 100000
+]
 
-
-
-save_to_json(train_data, "en_coNER_train.json")
-save_to_json(validation_data, "en_coNER_validation.json")
+# save_to_json(train_data, "en_coNER_train.json")
+# save_to_json(validation_data, "en_coNER_validation.json")
 save_to_json(test_data, "en_coNER_test.json")
