3918ac23743397bb57f9725ae27b150c39d36c48
Cal Blanco <calblanco1125@gmail.com>
2024-11-26 14:26:05 -0800

    modularized some of the GRU code to contain fit, and prediction code. Created generic dataset class that can be used outside of each model, created a Collator class which allows us to dynamically generate a collate_fn (primarily used by the GRU's at this point)

--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
 from data_util import retrieve_data
 from named_entity_recognition import ner_baseline
 from database import query_by_name, query_by_instance, query_by_id
+from datasets import TagingDataset
+from gru_model import GRUModel
+from torch.utils.data import DataLoader
+from collaters import Collator
+from data_util import retrieve_coNER
+from sklearn.model_selection import train_test_split
+
+BATCH_SIZE = 64
+
+x_train, y_train = retrieve_coNER("en_coNER_train.json")
+x_val, y_val = retrieve_coNER("en_coNER_validation.json")
+
+x_val, x_test, y_val, y_test = train_test_split(x_val,y_val, test_size=0.5, random_state=64)
+
+train_dataset = TagingDataset(x_train,y_train, training=True)
+val_dataset = TagingDataset(x_val, y_val, token_vocab=train_dataset.token_vocab, tag_vocab=train_dataset.tag_vocab, training=False)
+test_dataset = TagingDataset(x_test, y_test, token_vocab=train_dataset.token_vocab, tag_vocab=train_dataset.tag_vocab, training=False)
 
 
-out = ner_baseline("fr", num_samples=10) #generate NER baseline results 
-print(out)
+collater = Collator(train_dataset.token_vocab, train_dataset.tag_vocab)
 
-ids = [] #Create a list of just the ids we should have recieved
-for item in out:
-    ids.extend(item[1])
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collater.collate_fn)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collater.collate_fn)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collater.collate_fn)
 
-print(query_by_id(ids, 'wiki_id, name, instance_of, fr')) #print out query results
+gru = GRUModel(train_dataset.token_vocab, train_dataset.tag_vocab)
+
+gru.fit(train_loader, val_loader, epochs=1)
+
+out = gru.predict(test_loader)
+
+decode = gru.decode(out)
+
+print(decode[0])
+
+
