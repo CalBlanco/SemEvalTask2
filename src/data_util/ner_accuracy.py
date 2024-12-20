b4e2df875d5a525ae36f86c7ee72ffd872371928
@j.stclair <jackstclair@rocketmail.com>
2024-12-19 20:54:50 -0800

    updated ner accuracy to better reflect the task

--- src/data_util/ner_accuracy.py
+++ src/data_util/ner_accuracy.py
@@ -18,31 +18,27 @@ def ner_accuracy(predictions, targets) -> float:
     incorrect = 0
     sentence_count = 0
     data = []
-    for sentence in predictions:
-        entity_count = 0
+    for sentence in targets:
         if sentence == []:
-            if targets[sentence_count] == []:
+            if predictions[sentence_count] == []:
                 correct += 1
             else:
                 incorrect += 1
             sentence_count += 1
             continue
         for entity in sentence:
-            if targets[sentence_count] == []:
+            if predictions[sentence_count] == []:
                 incorrect += 1
-            elif entity_count >= len(targets[sentence_count]):
-                for entity_target in targets[sentence_count]:
-                    if fuzz.ratio(entity[0], entity_target[0]) > 80:
-                        correct += 1
-                    else:
-                        incorrect += 1
             else:
-                data.append((entity, targets[sentence_count][entity_count]))
-                if fuzz.ratio(entity[0], targets[sentence_count][entity_count][0]) > 80:
-                    correct += 1
-                else:
+                found_entity = False
+                for entity_target in predictions[sentence_count]:
+                    if fuzz.ratio(entity[0], entity_target[0]) > 80:
+                        found_entity = True
+                        data.append((entity, entity_target))
+                        correct += 1
+                if not found_entity:
                     incorrect += 1
-            entity_count += 1
+                    data.append((entity, predictions[sentence_count]))
         sentence_count += 1
     print(f"Correct: {correct}, Incorrect: {incorrect}")
     accuracy = correct/(correct+incorrect)
@@ -68,41 +64,28 @@ def class_accuracy(predictions, targets) -> float:
     incorrect = 0
     sentence_count = 0
     entity_types = {}
-    for sentence in predictions:
-        entity_count = 0
+    for sentence in targets:
         if sentence == []:
-            if targets[sentence_count] == []:
+            if predictions[sentence_count] == []:
                 correct += 1
             else:
                 incorrect += 1
             sentence_count += 1
             continue
         for entity in sentence:
-            if targets[sentence_count] == []:
+            if predictions[sentence_count] == []:
                 entity_types[f"None_incorrect"] = entity_types.get(f"None_incorrect", 0) + 1
                 incorrect += 1
-            elif entity_count >= len(targets[sentence_count]):
-                for entity_target in targets[sentence_count]:
+            else:
+                found_entity = False
+                for entity_target in predictions[sentence_count]:
                     if fuzz.ratio(entity[1], entity_target[1]) > 80:
                         entity_types[f"{entity[1]}_correct"] = entity_types.get(f"{entity[1]}_correct", 0) + 1
+                        found_entity = True
                         correct += 1
-                    else:
-                        if entity[1] == "":
-                            entity_types[f"None_incorrect"] = entity_types.get(f"None_incorrect", 0) + 1
-                        else:
-                            entity_types[f"{entity[1]}_incorrect"] = entity_types.get(f"{entity[1]}_incorrect", 0) + 1
-                        incorrect += 1
-            else:
-                if fuzz.ratio(entity[1], targets[sentence_count][entity_count][1]) > 80:
-                    entity_types[f"{entity[1]}_correct"] = entity_types.get(f"{entity[1]}_correct", 0) + 1
-                    correct += 1
-                else:
-                    if entity[1] == "":
-                        entity_types[f"None_incorrect"] = entity_types.get(f"None_incorrect", 0) + 1
-                    else:
-                        entity_types[f"{entity[1]}_incorrect"] = entity_types.get(f"{entity[1]}_incorrect", 0) + 1
+                if not found_entity:
+                    entity_types[f"{entity[1]}_incorrect"] = entity_types.get(f"{entity[1]}_incorrect", 0) + 1
                     incorrect += 1
-            entity_count += 1
         sentence_count += 1
     print(f"Correct: {correct}, Incorrect: {incorrect}")
     accuracy = correct/(correct+incorrect)
